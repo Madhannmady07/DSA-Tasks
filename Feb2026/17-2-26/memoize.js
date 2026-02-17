952a491d34500731ae32a9867cd7f2a7e701a5f9
Madhan Kumar K <madhankumark763@gmail.com>
2026-02-17 16:18:58 +0530

    Solved today's problems - 17/02/2026

---
+++ Feb2026/17-2-26/memoize.js
@@ -0,0 +1,32 @@
+// leetcode: 2623
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache= {};
+    return function(...args) {
+        let key = JSON.stringify(args);
+
+        if(key in cache){
+            return cache[key]
+        }
+
+        let res = fn.apply(this, args);
+        cache[key] = res;
+        return res;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
